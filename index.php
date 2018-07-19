425681b5d9f9917e3ce962eb147d74eeb43c2821
Felipe de Moraes <moraesfd@gmail.com>
2018-07-18 22:45:11 -0300

    home page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include_once('partials/_header.php'); ?>
+
+    <section class="home">
+        <div class="container text-center">
+            <div class="top">
+                <img src="assets/images/falcon.png" alt="">
+            </div>
+            <div class="bottom">
+                <h1>StarQuiz!</h1>
+                <a href="partials/game.php">JOGAR</a>
+            </div>
+        </div>
+    </section>
+
+<?php include_once('partials/_footer.php'); ?>
---
+++ partials/game.php
@@ -0,0 +1,13 @@
+<?php include_once('../partials/_header.php'); ?>
+
+<div class="container">
+    <div class="top">
+        <img src="/assets/images/falcon.png" alt="">
+    </div>
+    <div class="bottom">
+        <h1>StarQuiz!</h1>
+        <a href=""></a>
+    </div>
+</div>
+
+<?php include_once('../partials/_footer.php'); ?>
